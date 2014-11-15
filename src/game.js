646a8ef0cfed20426fa3405d56a0d7810ca8cace
Ivan Krechetov <ivan.krechetov@gmail.com>
2014-11-15 13:47:07 +0100

    Delete all the code

--- src/game.js
+++ src/game.js
@@ -1,13 +1,3 @@
 (function () {
     'use strict';
-
-    exports.isAlive = function (world, row, column) {
-        return world[row][column] === 1;
-    };
-
-    exports.nbDeltas = function () {
-        return [[1,2],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]];
-    };
-
-    exports.samePair = function (p1, p2) {return p1[0] === p2[0] && p1[1] === p2[1];};
 }());
