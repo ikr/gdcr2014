fc4b42311a6322148175ef0f05d4fc153567c8db
Ivan Krechetov <ivan.krechetov@gmail.com>
2014-11-15 12:39:02 +0100

    30 sec

--- src/game.js
+++ src/game.js
@@ -1,7 +1,13 @@
 (function () {
     'use strict';
 
-    module.exports = function (x) {
-        return x;
+    exports.isAlive = function (world, row, column) {
+        return world[row][column] === 1;
     };
+
+    exports.nbDeltas = function () {
+        return [[1,2],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]];
+    };
+
+    exports.samePair = function (p1, p2) {return p1[0] === p2[0] && p1[1] === p2[1];};
 }());
--- tests/game.spec.js
+++ tests/game.spec.js
@@ -2,13 +2,52 @@ describe('dummy', function () {
     'use strict';
 
     var assert = require('assert'),
-        dummy = require('../src/game');
+        g = require('../src/game');
 
-    it('is a function', function () {
-        assert.strictEqual(typeof dummy, 'function');
+    describe('isAlive', function () {
+        it('isAlive is true in trivial case', function () {
+            assert(g.isAlive([[1]], 0, 0));
+        });
+
+        it('isAlive is false for a dead cell', function () {
+            assert(!g.isAlive([[0]], 0, 0));
+        });
+
+        it('2 columns', function () {
+            assert(g.isAlive([[0, 1]], 0 ,1));
+        });
     });
 
-    it('is identical', function () {
-        assert.strictEqual(dummy('moo'), 'moo');
+    describe('nbDeltas', function () {
+            var all = g.nbDeltas(0, 0);
+
+        it('nbDeltas has 8 things', function () {
+            assert.strictEqual(all.length, 8);
+        });
+
+        it('nbDeltas returns pairs', function () {
+            assert.strictEqual(all[0].length, 2);
+        });
+
+        it('nbDeltas returns pairs', function () {
+
+            all.forEach(function (pair) {
+                assert.strictEqual(pair.length, 2);
+            });
+        });
+
+        it('samePair is true for same', function () {
+            assert(!g.samePair(all[0], all[1]));
+        });
+    });
+
+    describe('samePair', function () {
+        it('samePair is true for same', function () {
+            assert(g.samePair([0, 0], [0, 0]));
+        });
+
+        it('samePair is true for same', function () {
+            assert(!g.samePair([0, 1], [0, 0]));
+        });
     });
 });
